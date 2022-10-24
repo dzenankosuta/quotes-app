41abafbc3116d1e06c17f904d8b0b98b870b8f99
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-10-24 14:35:14 +0200

    useEffect for login page YEEEEESSSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function App() {
-  return (
-    <div>
-      App Works!
-    </div>
-  );
+  const [accessToken, setAccessToken] = useState(null);
+  useEffect(() => {
+    axios
+      .post("http://localhost:8000/sessions", {
+        username: "zika",
+        password: "1234",
+      })
+      .then((response) => {
+        console.log(response.data.accessToken);
+        setAccessToken(response.data.accessToken);
+        window.localStorage.setItem("accessToken", accessToken);
+        // console.log(accessToken);
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          console.log(`Invalid credentials!`);
+          setAccessToken(null);
+          window.localStorage.setItem("accessToken", accessToken);
+          // console.log(accessToken);
+        } else {
+          console.log(error);
+          setAccessToken(null);
+          window.localStorage.setItem("accessToken", accessToken);
+          // console.log(accessToken);
+        }
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [accessToken]);
+  return <div>App Works!</div>;
 }
 
 export default App;
