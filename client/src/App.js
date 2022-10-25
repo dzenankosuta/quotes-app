8487a40131b197ce9d0c2b1eaa3193a03995ad9e
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-10-25 17:15:16 +0200

    adding logo, NavBar, logout button and so on

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import { Route, Routes } from "react-router-dom";
 import Quotes from "./pages/Quotes/Quotes";
+import NavBar from "./NavBar/NavBar";
 
 function App() {
   return (
     <div>
-      App Works!
+      <NavBar />
       <Routes>
         <Route path={"/"} element={<LoginPage />} />
         <Route path={"/quotes"} element={<Quotes />} />
---
+++ client/src/NavBar/NavBar.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { TokenContext } from "../context/TokenContext";
+import "./NavBar.css";
+
+const NavBar = () => {
+  const { handleLogout } = useContext(TokenContext);
+  const activeStyle = {
+    padding: "0.6rem",
+    borderRadius: "0.5rem",
+    backgroundColor: "#f0f8ff",
+    color: "#212926",
+  };
+  const classicStyle = {
+    backgroundColor: "#7ba798",
+    color: "#f0fffa",
+  };
+  return (
+    <div className="navbar">
+      <NavLink
+        style={{ height: "100%", position: "absolute", left: "3rem" }}
+        to="/"
+      >
+        <img src={require("../images/logo2.png")} alt="logo" className="img" />
+      </NavLink>
+      <NavLink
+        className="link"
+        to="/quotes"
+        style={({ isActive }) => (isActive ? activeStyle : classicStyle)}
+      >
+        QUOTES
+      </NavLink>
+      <button className="button-39" onClick={handleLogout}>
+        LOGOUT
+      </button>
+    </div>
+  );
+};
+
+export default NavBar;
