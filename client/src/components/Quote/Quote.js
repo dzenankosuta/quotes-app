e19f60230662e0e80cc421c78346bce7c4616b7a
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-10-28 12:18:13 +0200

    Adding color for percent depending on the number

--- client/src/components/Quote/Quote.js
+++ client/src/components/Quote/Quote.js
@@ -5,11 +5,23 @@ const Quote = ({ content, authorName, upvotesCount, downvotesCount }) => {
   const totalPercent = 100;
   const percentPerVote = totalPercent / (upvotesCount + downvotesCount);
   const upvotesPercent = Math.round(upvotesCount * percentPerVote);
+  const color =
+    upvotesPercent <= 20
+      ? "color1"
+      : upvotesPercent > 20 && upvotesPercent <= 40
+      ? "color2"
+      : upvotesPercent > 40 && upvotesPercent <= 60
+      ? "color3"
+      : upvotesPercent > 60 && upvotesPercent <= 80
+      ? "color4"
+      : upvotesPercent > 80 && upvotesPercent <= 95
+      ? "color5"
+      : "color6";
   return (
     <div className="quote">
       <div className="left">
         <button className="btn2">&#129081;</button>
-        <p className="percent">{upvotesPercent}%</p>
+        <p className={`percent ${color}`}>{upvotesPercent}%</p>
         <p className="ratio">
           {upvotesCount} / {downvotesCount}
         </p>
