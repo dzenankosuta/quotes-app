be3401ec295f1a1240db737a8878f85fcd71070a
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-10-29 12:53:33 +0200

    finishing post upvote and delete upvote for quote

--- client/src/components/Quote/Quote.js
+++ client/src/components/Quote/Quote.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Quote.css";
 import axios from "axios";
 
@@ -10,9 +10,12 @@ const Quote = ({
   downvotesCount,
   givenVote,
 }) => {
+  const [vote, setVote] = useState(givenVote);
+  const [upVotesCount, setupVotesCount] = useState(upvotesCount);
+  const [downVotesCount, setdownVotesCount] = useState(downvotesCount);
   const totalPercent = 100;
-  const percentPerVote = totalPercent / (upvotesCount + downvotesCount);
-  const upvotesPercent = Math.round(upvotesCount * percentPerVote);
+  const percentPerVote = totalPercent / (upVotesCount + downvotesCount);
+  const upvotesPercent = Math.round(upVotesCount * percentPerVote);
   const color =
     upvotesPercent <= 20
       ? "color1"
@@ -25,9 +28,8 @@ const Quote = ({
       : upvotesPercent > 80 && upvotesPercent <= 95
       ? "color5"
       : "color6";
-  let classBtnVote1 = givenVote === "upvote" ? "upvote-class" : "vote-class";
-  let classBtnVote2 =
-    givenVote === "downvote" ? "downvote-class" : "vote-class";
+  let classBtnVote1 = vote === "upvote" ? "upvote-class" : "vote-class";
+  let classBtnVote2 = vote === "downvote" ? "downvote-class" : "vote-class";
 
   const postUpvote = () => {
     axios
@@ -37,31 +39,54 @@ const Quote = ({
         },
       })
       .then((response) => {
-        console.log(response);
+        // console.log(response.data.givenVote);
+        setupVotesCount(upVotesCount + 1);
+        setVote(response.data.givenVote);
+      })
+      .catch((error) => {
+        // console.log(error)
+      });
+  };
+  const deleteUpvote = () => {
+    axios
+      .delete(`http://localhost:8000/quotes/${id}/upvote`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        console.log(response.data.givenVote);
+        setupVotesCount(upVotesCount - 1);
+        setVote(response.data.givenVote);
       })
       .catch((error) => console.log(error));
   };
-  const defaultFunc = () => {
-    console.log("hi");
-  };
+
   return (
     <div className="quote">
       <div className="left">
         <button
+          disabled={vote === "downvote"}
           className={`btn2 ${classBtnVote1}`}
-          onClick={() => (givenVote === "none" ? postUpvote() : defaultFunc())}
+          onClick={() =>
+            vote === "none"
+              ? postUpvote()
+              : vote === "upvote"
+              ? deleteUpvote()
+              : () => {}
+          }
         >
           &#129081;
         </button>
         <p className={`percent ${color}`}>{upvotesPercent}%</p>
         <p className="ratio">
-          {upvotesCount} / {downvotesCount}
+          {upVotesCount} / {downvotesCount}
         </p>
         <button className={`btn2 ${classBtnVote2}`}>&#129083;</button>
       </div>
       <div className="right">
         <p className="content">{content}</p>
-        <p>{givenVote}</p>
+        <p>{vote}</p>
         <p>{id}</p>
         <p className="author">Author: {authorName}</p>
       </div>
